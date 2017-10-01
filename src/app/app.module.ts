cad5897cd02cde011a9dbdd4fbe48a9745783a6b
Gilberto <gr.martins@gmail.com>
2017-10-01 12:45:51 -0300

    Primeira versão do front-end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Importa todos os componentes do primeng, inclusive os que não estão sendo utilizados
+// import { InputTextModule } from 'primeng/primeng';
+// Importa somente o componente inputtext
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { ButtonModule } from 'primeng/components/button/button';
+import { DataTableModule } from 'primeng/components/datatable/datatable';
+import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +16,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    ButtonModule,
+    DataTableModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
